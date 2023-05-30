d9fbf4c58f6856eb6bfb8ffa46d80f223be30fd2
Nasser Hadi <Naseerhadie@gmail.com>
2023-05-30 20:27:53 +0300

    first commit

---
+++ new project/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using new_project.Models;
+using System.Diagnostics;
+
+namespace new_project.Controllers
+{
+    public class HomeController : Controller
+    {
+     
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Services()
+        {
+            return View();
+        }
+
+        public IActionResult Untree()
+        {
+            return View();
+        }
+
+    }
+}
